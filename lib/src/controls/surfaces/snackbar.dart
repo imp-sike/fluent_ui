3317ce436bedc81a0ea9c040a59b21d08ed3a7ce
Sulav Parajuli <93368594+imp-sike@users.noreply.github.com>
2022-12-06 12:21:27 +0000

    Snackbar Duration Fixed

--- lib/src/controls/surfaces/snackbar.dart
+++ lib/src/controls/surfaces/snackbar.dart
@@ -1,8 +1,8 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/foundation.dart';
 
-const Duration snackbarShortDuration = Duration(seconds: 2);
-const Duration snackbarLongDuration = Duration(seconds: 2);
+const Duration snackbarShortDuration = Duration(seconds: 1.5);
+const Duration snackbarLongDuration = Duration(seconds: 2.75);
 
 /// Shows a snackbar on the given context.
 ///
